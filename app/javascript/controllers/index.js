840f917df595f407fa945a383929c95c9b90105c
Pierre-Elie ABERGEL <pe.abergel@gmail.com>
2025-01-09 18:57:26 +0100

    End brand carousel swiper

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,11 +7,13 @@ import ScrollReveal from '@stimulus-components/scroll-reveal'
 // Register the above dropdown controller with Stimulus.
 import CardController from "./card_controller";
 import FadeinController from "./fadein_controller";
-// import CarouselFadeController from "./carousel_fade_controller";
+import CarouselFadeController from "./carousel_fade_controller";
+import CarouselSlideController from "./carousel_slide_controller";
 
 application.register("card", CardController)
 application.register("fadein", FadeinController)
-// application.register("carousel-fade", CarouselFadeController)
+application.register("carousel-fade", CarouselFadeController)
+application.register("carousel-slide", CarouselSlideController)
 application.register("notification", Notification)
 application.register('scroll-reveal', ScrollReveal)
 
