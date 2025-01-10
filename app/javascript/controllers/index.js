7b2cf7cb47e659925a5499f2775c0aec27f741d6
Kevin Abergel <k.abergel91@gmail.com>
2025-01-10 12:43:04 +0100

    remove useless imports stimulus controllers

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,18 +1,4 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-import Notification from '@stimulus-components/notification'
-import ScrollReveal from '@stimulus-components/scroll-reveal'
-
-// Register the above dropdown controller with Stimulus.
-import CardController from "./card_controller";
-import FadeinController from "./fadein_controller";
-import SwiperController from "./swiper_controller";
-
-application.register("card", CardController)
-application.register("fadein", FadeinController)
-application.register("swiper", SwiperController)
-application.register("notification", Notification)
-application.register('scroll-reveal', ScrollReveal)
-
 eagerLoadControllersFrom("controllers", application)
