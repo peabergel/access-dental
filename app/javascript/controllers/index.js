be7ea4ffd57996f66258c1c69d1496e88c59a467
Kevin Abergel <k.abergel91@gmail.com>
2025-01-10 13:08:20 +0100

    fix relatives paths controllers stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,10 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import Notification from '@stimulus-components/notification'
+import ScrollReveal from '@stimulus-components/scroll-reveal'
+
+application.register("notification", Notification)
+application.register('scroll-reveal', ScrollReveal)
+
 eagerLoadControllersFrom("controllers", application)
