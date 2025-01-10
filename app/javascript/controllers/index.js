24f48ff7b5ce63fea814568f2ae154f013c84864
Pierre-Elie ABERGEL <pe.abergel@gmail.com>
2025-01-10 11:57:50 +0100

    fix conflicts

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,21 +7,11 @@ import ScrollReveal from '@stimulus-components/scroll-reveal'
 // Register the above dropdown controller with Stimulus.
 import CardController from "./card_controller";
 import FadeinController from "./fadein_controller";
-<<<<<<< HEAD
-import CarouselFadeController from "./carousel_fade_controller";
-import CarouselSlideController from "./carousel_slide_controller";
-
-application.register("card", CardController)
-application.register("fadein", FadeinController)
-application.register("carousel-fade", CarouselFadeController)
-application.register("carousel-slide", CarouselSlideController)
-=======
 import SwiperController from "./swiper_controller";
 
 application.register("card", CardController)
 application.register("fadein", FadeinController)
 application.register("swiper", SwiperController)
->>>>>>> 464782b484b120907cadc7a1dd311aca5857485f
 application.register("notification", Notification)
 application.register('scroll-reveal', ScrollReveal)
 
