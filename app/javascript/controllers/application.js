ae78f78b2442feca376edd458bdf433829fa13bc
Pierre-Elie ABERGEL <pe.abergel@gmail.com>
2025-01-12 18:23:30 +0100

    Testing carousel

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -15,3 +15,26 @@ document.addEventListener("turbo:load", () => {
 document.addEventListener("turbo:render", () => {
   HSStaticMethods.autoInit();
 });
+
+const carousels = document.querySelectorAll('.carousel-target'); // Select the carousel elements
+
+carousels.forEach(carousel => {
+  let startX, startY;
+
+  carousel.addEventListener('touchstart', (e) => {
+    startX = e.touches[0].pageX;
+    startY = e.touches[0].pageY;
+  });
+
+  carousel.addEventListener('touchmove', (e) => {
+    const moveX = e.touches[0].pageX;
+    const moveY = e.touches[0].pageY;
+    const diffX = Math.abs(moveX - startX);
+    const diffY = Math.abs(moveY - startY);
+
+    // If the vertical swipe distance is greater than the horizontal swipe distance, prevent the carousel swipe
+    if (diffY > diffX) {
+      e.stopPropagation(); // Stops the carousel from intercepting the swipe
+    }
+  });
+});
