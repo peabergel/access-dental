543cc1d0afefe0f3e1459f6701d25c9ca9a9d434
Pierre-Elie ABERGEL <pe.abergel@gmail.com>
2025-01-13 16:09:17 +0100

    Testing sending mail

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,10 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: "smtp-relay.brevo.com",
-  port: "587",
-  domain: "scalingo.com",
-  user_name: ENV["BREVO_API_TOKEN"],
-  password: ENV["BREVO_API_TOKEN"],
-  # authentification: (ici le wagon met :cram_md5)
-  authentication:  "plain",
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: ENV["GMAIL_USERNAME"], # Ton email Gmail
+  password: ENV["GMAIL_PASSWORD"], # Ton mot de passe ou mot de passe d'application
+  authentication: "plain",
   enable_starttls_auto: true
 }
