b404ddae889c0c9ad8c3cc63a6a24c2bcf6dc6b7
Pierre-Elie ABERGEL <pe.abergel@gmail.com>
2025-02-01 14:52:37 +0100

    End Section screen

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,6 @@ pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "preline", to: "https://cdn.jsdelivr.net/npm/preline@2.5.0/dist/preline.min.js"
-pin "process" # @2.1.0
 pin "stimulus-use" # @0.52.3
 pin "@stimulus-components/notification", to: "@stimulus-components--notification.js" # @3.0.0
 pin "@stimulus-components/scroll-reveal", to: "@stimulus-components--scroll-reveal.js" # @4.0.0
@@ -14,5 +13,5 @@ pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.11
 pin "sortablejs" # @1.15.6
 pin "animejs" # @3.2.2
 pin "mapbox-gl" # @3.9.3
-pin "simple-parallax-js" # @6.0.1
-pin "react" # @19.0.0
+# pin "countup", to: "countUp.umd.js"
+pin "railsui-stimulus" # @1.0.8
