5fba5d0f9287e483fdcd46561c4756f01633cf88
Pierre-Elie Abergel <pe.abergel@gmail.com>
2025-11-19 17:03:54 +0100

    Remove unused Articles helper code

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,9 +1,2 @@
 module ArticlesHelper
-  def pdf_image_or_image(article)
-    image = image_tag article.image_url,
-      alt: "#{article.title} image",
-      class: "w-full h-56 sm:h-64 object-cover transform transition duration-500 hover:scale-110 rounded-[2rem]"
-
-    article.pdf_url.present? ? link_to(image, article.pdf_url, target: "_blank") : image
-  end
 end
